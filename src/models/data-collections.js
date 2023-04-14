61308225d07e93ef622c8ef0ee56f03ee5441cdf
Thomas Lau <tomslau@outlook.com>
2023-04-14 13:28:30 -0700

    test

--- src/models/data-collections.js
+++ src/models/data-collections.js
@@ -7,29 +7,50 @@ class DataCollection {
   }
 
   get(id) {
-    if (id) {
-      return this.model.findOne({ where: { id } });
-    }
-    else {
-      return this.model.findAll({});
+    try{
+      if (id) {
+        return this.model.findOne({ where: { id } });
+      }
+      else {
+        return this.model.findAll({});
+      }
+
+    } catch(e){
+      console.error(e);
     }
   }
 
   create(record) {
+    try{
     return this.model.create(record);
+  } catch(e){
+    console.error(e);
+  }
   }
 
   update(id, data) {
+    try{
     return this.model.findOne({ where: { id } })
       .then(record => record.update(data));
+    } catch(e){
+      console.error(e);
+    }
   }
 
   delete(id) {
+    try{
     return this.model.destroy({ where: { id }});
+  } catch(e){
+    console.error(e);
+  }
   }
   patch(id){
+    try{
     return this.model.findOne({ where: { id } })
     .then(record => record.update(data));
+  } catch(e){
+    console.error(e);
+  }
   }
 }
 
