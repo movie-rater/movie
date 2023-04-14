7d6026a83fcfe18a8e4c2340f830bfcc18150f80
Thomas Lau <tomslau@outlook.com>
2023-04-14 12:42:53 -0700

    changeback

--- src/models/movie.js
+++ src/models/movie.js
@@ -3,18 +3,7 @@
 const movieModel = (sequelize, DataTypes) => sequelize.define('Movies', {
   title: { type: DataTypes.STRING, required: true },
   rating: {
-    type: DataTypes.INTEGER, required: true,
-    validate: {
-      max: {
-        args: [13],
-        msg: "Maximum 13 in rating"
-      },
-      min: {
-        args: [0],
-        msg: "Minimum 0 in rating"
-      }
-    }
-  }
+    type: DataTypes.INTEGER, required: true }
 });
 
 module.exports = movieModel;
